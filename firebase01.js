88454a5adb713635a6a496725571965da993bded
kumjay061 <144765557+kumjay061@users.noreply.github.com>
2024-08-08 11:44:51 +0530

    Update firebase01.js

--- firebase01.js
+++ firebase01.js
@@ -1,12 +1,12 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC90GnQYUR1ysJYT7bX0r1bDIllxMo83w4",
-  authDomain: "getupbaba1.firebaseapp.com",
-  databaseURL: "https://getupbaba1-default-rtdb.firebaseio.com",
-  projectId: "getupbaba1",
-  storageBucket: "getupbaba1.appspot.com",
-  messagingSenderId: "465709312280",
-    appId: "1:465709312280:web:8696dbe25d315724e6566d"
+    apiKey: "AIzaSyC4WBGciZ1sM3EsMYvp7GMwDRx6425NESg",
+    authDomain: "cgoodwork01.firebaseapp.com",
+    databaseURL: "https://cgoodwork01-default-rtdb.firebaseio.com",
+    projectId: "cgoodwork01",
+    storageBucket: "cgoodwork01.appspot.com",
+    messagingSenderId: "439218285657",
+    appId: "1:439218285657:web:ad24fe9a5d2578c8b31548"
  
   };
 
